e51324d42efaaf27bdbdc18e993ec01f727eeadd
Arthur Gl <arthur.pro.gontier@gmail.com>
2026-03-04 10:55:36 +0000

    updated tableplots

--- tablereadplot.jl
+++ tablereadplot.jl
@@ -2,9 +2,9 @@ function plotresultstablenature(file)
     table = Vector{Vector{Any}}()
     legende = Vector{String}()
     n = 0
-    file = string(pwd(),'/',file)
-    println(file)
-    open(file, "r") do f
+    pfile = string(pwd(),'/',file)
+    println(pfile)
+    open(pfile, "r") do f
         firstline = readline(f)
         legende = split(firstline,' ')
         n = length(legende)
@@ -17,20 +17,33 @@ function plotresultstablenature(file)
             push!(table,t)
         end
     end
-    # veripb elaborate vs trimming elaborate
-    # printpoints2Dlog(table,2,8)
-    # veri vs trim
-    printpoints2Dlog(table,5,11)
+    printpoints2D(table,2,8,"elab")
+    printpoints2D(table,5,11,"verif")
+    printpoints2Dlog(table,2,8,"elab")
+    printpoints2Dlog(table,5,11,"verif")
 end
-function printpoints2D(table,a,b)
+function maxvalue(table,a)
+    m = 0
+    for t in table 
+        if t[a]!==nothing
+            if t[a]>m m=t[a] end
+        end
+    end
+    return m
+end
+function printpoints2D(table,a,b,title)
+    prefixtikz(maxvalue(table,a),maxvalue(table,b),title,title)
     for t in table 
         if t[a]!==nothing &&t[b]!==nothing
             print(t[a],'/',t[b],',')
         end
     end
     println()
+    postfixtikz()
 end
-function printpoints2Dlog(table,a,b)
+function printpoints2Dlog(table,a,b,title)
+    title = string(title," (log)")
+    prefixtikz(logsmooth(maxvalue(table,a)),logsmooth(maxvalue(table,b)),title,title)
     for t in table 
         if t[a]!==nothing &&t[b]!==nothing
             print(
@@ -39,8 +52,33 @@ function printpoints2Dlog(table,a,b)
         end
     end
     println()
+    postfixtikz()
 end
 function logsmooth(a)
-    return round(max(log10(a),0),digits = 3)
+    return round(max(log10(a),0),sigdigits = 3)
+end
+function prefixtikz(mx=10,my=10,xlbl="",ylbl="")
+    m = max(mx,my)
+    steps = Int(m÷10 + 1)
+    m = Int((m÷10)*10 + 10) # to make a 10 integer scale
+    mx = my = m
+    xsteps = steps
+    ysteps = steps
+    scale = 1#/max(xsteps,ysteps)
+    xx = 1/xsteps
+    yy = 1/ysteps    # mx = Int(ceil(mx))
+println("\\begin{tikzpicture}[scale=$scale, x=$xx cm, y=$yy cm]
+  \\def\\xmin{0} \\def\\xmax{$mx} \\def\\ymin{0} \\def\\ymax{$my}
+  \\draw[style=help lines, ystep=$ysteps, xstep=$xsteps] (\\xmin,\\ymin) grid (\\xmax,\\ymax);
+  \\draw[->] (\\xmin,\\ymin) -- (\\xmax,\\ymin) node[above left] {$xlbl};
+  \\draw[->] (\\xmin,\\ymin) -- (\\xmin,\\ymax) node[below right] {trimmed $ylbl};
+  \\foreach \\x in {0,$xsteps,...,\\xmax} \\node at (\\x, \\ymin) [below] {\\x};
+  \\foreach \\y in {0,$ysteps,...,\\xmax} \\node at (\\xmin,\\y) [left] {\\y};
+\\draw (\\xmin,\\ymin) -- (\\xmax,\\ymax) ;
+\\foreach \\x/\\y in{")
+end
+function postfixtikz()
+println("}\\draw (\\x,\\y) node[noeudver,fill opacity=0.75,scale=0.3] {}; 
+\\end{tikzpicture}")
 end
 plotresultstablenature(ARGS[1])
